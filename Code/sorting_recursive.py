0634ce8d9efcc015913b67e9b1598bff04ec298e
Thomas <thomaslee9474@gmail.com>
2019-10-31 15:59:25 -0700

    [update] docstring

--- Code/sorting_recursive.py
+++ Code/sorting_recursive.py
@@ -105,18 +105,31 @@ def merge_sort(items: [int]) -> [int]:
         return merge(sorted_first_half, sorted_second_half)
 
 
-def partition(items, low, high):
-    """Return index `p` after in-place partitioning given items in range
-    `[low...high]` by choosing a pivot (TODO: document your method here) from
-    that range, moving pivot into index `p`, items less than pivot into range
-    `[low...p-1]`, and items greater than pivot into range `[p+1...high]`.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
+def partition(items: [int], low: int, high: int):
+    """
+        Return index `p` after in-place partitioning given items in range
+        `[low...high]` by choosing a pivot (low) from
+        that range, moving pivot into index `p`, items less than pivot into range
+        `[low...p-1]`, and items greater than pivot into range `[p+1...high]`.
+
+        Args:
+            items: list of ints.
+            low: pivot.
+            high: length of list.
+
+    """
+
+    # TODO: Running time: ??? Why and under what conditions?
+    # TODO: Memory usage: ??? Why and under what conditions?
+
     # TODO: Choose a pivot any way and document your method in docstring above
     # TODO: Loop through all items in range [low...high]
     # TODO: Move items less than pivot into front of range [low...p-1]
     # TODO: Move items greater than pivot into back of range [p+1...high]
     # TODO: Move pivot item into final position [p] and return index p
+
+    # split list into rough halves
+    middle_index = len(items) // 2
 
 
 def quick_sort(items, low=None, high=None):
